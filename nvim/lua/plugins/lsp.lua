df81e80fc1c93c5f08b6181bc8771dde5ede5ad4
bigelle <ruslan.ok04@gmail.com>
2025-12-27 13:31:59 +0500

    fix cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,32 +12,43 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
-                sources = cmp.config.sources({
+                sources = {
                     { name = 'nvim_lsp' },
-                }, {
                     { name = 'buffer' },
-                })
+                }
             })
         end
     },
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			vim.lsp.enable({
-				"gopls",
-				"lua_ls",
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable({
+                "gopls",
+                "lua_ls",
                 "rust_analyzer"
-			})
-		end
-	},
-	{
-		"mason-org/mason.nvim",
-		opts = {}
-	},
+            })
+        end
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
 }
